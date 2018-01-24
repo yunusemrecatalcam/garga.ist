f0b7e7e7f544f62230e4e9738525356053e52b58
Monster <yunusemrecatalcam@gmail.com>
2018-01-24 23:40:02 +0300

    php mysql insertion ok

--- connect.php
+++ connect.php
@@ -1,10 +1,12 @@
 <?php
+define('HOST','localhost');
+define('USER','root') ;
+define('DB','gargaiafl');
+define('PASS','qwerty');
+
 function open_con(){
 
-  define('HOST','localhost');
-  define('USER','root') ;
-  define('DB','gargaiafl');
-  define('PASS','qwerty');
+
   $conn = mysqli_connect(HOST,USER,PASS,DB);
 
   return $conn;
--- get_text.php
+++ get_text.php
@@ -6,5 +6,15 @@ include 'connect.php';
 if(open_con()){
   echo "connected";
 }
-header("Location: http://stackoverflow.com");
+$conn=open_con();
+
+//header("Location: http://stackoverflow.com");
+$mahlas= mysqli_real_escape_string($conn, $_POST['mahlas']);
+$metin= mysqli_real_escape_string($conn,$_POST['yazi']);
+
+$kayit=mysqli_query($conn,"INSERT INTO texts(mahlas,metin) VALUES ('$mahlas','$metin')");
+if($kayit){
+  echo "vuuuu";
+}
+
 ?>
