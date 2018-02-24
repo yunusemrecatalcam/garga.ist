0e8a2e0c5199d7066f5311f8142e835b8e77e306
Monster <yunusemrecatalcam@gmail.com>
2018-02-24 17:42:51 +0300

    shitty things happened

--- adminospanel/gettest.php
+++ adminospanel/gettest.php
@@ -1,4 +1,14 @@
 <?php
+
+include ($_SERVER['DOCUMENT_ROOT'].'/beta/connect_daddy.php');
+$conn=open_con();
+
 $qwe=$_GET['username'];
-echo $qwe." holy holy holy shit";
+
+echo $qwe." holy holy holy shit oksholy holy holy shit oksholy holy holy shit oksholy holy holy shit oksholy holy holy shit oksholy holy holy shit oksholy holy holy shit oksholy holy holy shit oksholy holy holy shit oksholy holy holy shit oksholy holy holy shit oks";
+
+$cookie_name = "js";
+$cookie_value = "bakalim";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+
  ?>
