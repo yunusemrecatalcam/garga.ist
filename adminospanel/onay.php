bc20b650ffdc027a953ccb9d10fe5e82ee79a5e4
Monster <yunusemrecatalcam@gmail.com>
2018-03-11 22:47:15 +0300

    post method

--- adminospanel/onay.php
+++ adminospanel/onay.php
@@ -5,7 +5,10 @@ include ($_SERVER['DOCUMENT_ROOT'].'/beta/connect_daddy.php');
 $conn=open_con();
 mysqli_query($conn,"SET NAMES UTF8");
 
-$username= mysqli_real_escape_string($conn, $_GET['username']);
-$password= mysqli_real_escape_string($conn,$_GET['password']);
+$username= mysqli_real_escape_string($conn, $_POST['username']);
+$password= mysqli_real_escape_string($conn,$_POST['password']);
+$id= mysqli_real_escape_string($conn, $_POST['idsi']);
+
+echo $username." ".$password." ".$id;
 
  ?>
