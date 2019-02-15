2d4f91345b1c8937058eed34a360f0ca476595d8
yunusemrecatalcam <yunusemrecatalcam@gmail.com>
2019-02-15 23:18:47 +0300

    yaz

--- front/js/yaziver.js
+++ front/js/yaziver.js
@@ -6,23 +6,10 @@ function resize_form(){
   var topnav = document.getElementById("topnav");
   var collider =document.getElementsByClassName("collider");
   var len = (topnav.offsetHeight+100) + "px";
-/*
-  Array.from(collider).forEach(function(element){
-    element.style.top = len;
-    console.log(element.style.top);
-  });
-  collider.style.left = "100px";*/
-  //window.alert("sdf");
-}
-
-window.onclick = function(e) {
-  if (e.target.matches('.selection')) {
-    window.alert
-  }
 }
 
 $(document).ready(function(){
-  $("textarea").keypress(function(){
+  $("textarea").keydown(function(){
       var text = $('#metin').val();
       //window.alert(text);
       $('#paragr').html(text);
