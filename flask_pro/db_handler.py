8fee1d518881449e0a7e8c6d800a5c2ef28e403b
yunusemrecatalcam <yunusemrecatalcam@gmail.com>
2019-07-06 17:20:09 +0300

    waitlist, db ops

--- flask_pro/db_handler.py
+++ flask_pro/db_handler.py
@@ -1,6 +1,7 @@
 import mysql.connector
 
 class db_handler():
+
     def __init__(self):
         self.db= mysql.connector.connect(
             host="localhost",
@@ -8,8 +9,25 @@ class db_handler():
             passwd="pass",
             database="garga")
 
-        mycursor = self.db.cursor()
-        mycursor.execute("show tables")
+        self.cursor = self.db.cursor()
+        self.cursor.execute("SET NAMES 'utf8';")
+        self.cursor.execute("SET CHARACTER SET utf8;")
+        self.cursor.execute("show tables")
 
-        for x in mycursor:
+        for x in self.cursor:
             print(x)
+
+    def insert_text(self,textname,text,mahlas):
+        sql = "INSERT INTO texts (textname, text, mahlas) VALUES (%s, %s, %s)"
+        val = (textname, text, mahlas)
+        self.cursor.execute(sql, val)
+        self.db.commit()
+        print(self.cursor.rowcount," inserted")
+
+    def get_waitings(self):
+        sql = "SELECT * FROM texts"
+        self.cursor.execute(sql)
+        result = []
+        for waiter in self.cursor:
+            result.append(waiter)
+        return result
